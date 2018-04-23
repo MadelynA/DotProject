39889bd2351a810b5741519266020956839156e4
Madelyn Andersen <madelynv.andersen@gmail.com>
2018-04-23 11:22:59 -0400

    adds start of new prettier UI

--- DotProjectJava/src/userint/userInt.java
+++ DotProjectJava/src/userint/userInt.java
@@ -1,17 +1,40 @@
 package userint;
-import java.awt;
-//imports the basic AWT class to use for java GUI design
-public class userInt extends Frame{
-	
-	Panel menu = new Panel();
-	Panel gamePanel = new Panel();
-	//Creates frame and panels for UI
-	Button startButton = new Button();
-	Button quitButton = new Button();
-	menu.add(startButton);
-	menu.add(quitButton);
-	//adds buttons to menu
-	public userInt() {	}
-	
+import javafx.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public 
+public class UserInterface extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+ Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
+ public static void main(String[] args) {
+        new userInt();
+        userInt.start();
+    }
+
