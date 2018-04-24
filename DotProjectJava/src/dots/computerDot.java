6bc6a85e3070e006e33fd34eeefbc067843a1f6e
Madelyn Andersen <madelynv.andersen@gmail.com>
2018-04-24 11:33:43 -0400

    adds some to computerDot class

--- DotProjectJava/src/dots/computerDot.java
+++ DotProjectJava/src/dots/computerDot.java
@@ -3,16 +3,21 @@ import java.awt.Color;
 
 public class computerDot extends Dot{
 	
+	public ArrayList dotList;
+	//keeps track of list so it can delete itself when  it gets consumed
 	
-	public computerDot() {
-		
+	
+	public computerDot(int xCenter, int yCenter, Color color, int radius, int[] boardDimensions, ArrayList dotList) {
+		super(int xCenter, int yCenter, Color color, int radius, int[] boardDimensions);
+		this.dotList = dotList;
+		//xCenter and yCenter are randomly assigned from the side of the frame
+		//radius is randomly assigned
 	}
 	
 	public void delete() {
 		//has been eaten by user dot or exited the board -- this dot is deleted
+		dotList.remove(this);
 	}
-	
-	
 	
 	
 	
@@ -22,8 +27,8 @@ public class computerDot extends Dot{
 		
 	}
 	
-	public boolean collide(User_Dot u) {		
-		return false;
+	public boolean collide(computerDot dot) {		
+		//check if bounces off another dot of the same type -- tells movement to flip
 	}
 	
 	
