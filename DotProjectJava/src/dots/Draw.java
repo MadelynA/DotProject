a80339369199d0866de9223d268b50458a7cabb1
juliawu1 <julia.wu14@gmail.com>
2018-05-01 14:23:42 -0400

    added Draw class

---
+++ DotProjectJava/src/dots/Draw.java
@@ -0,0 +1,16 @@
+package dots;
+
+import java.awt.Color;
+import java.awt.geom.Ellipse2D;
+import java.awt.*;
+
+public class Draw {
+	public Draw(int[] points, Color c) {
+		
+	}
+	
+	public void animate() {
+		
+	}
+	
+}
