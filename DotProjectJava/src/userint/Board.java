2dc21c17d7c113f79431bcb120cf036c25be008a
Madelyn Andersen <madelynv.andersen@gmail.com>
2018-05-06 17:20:17 -0400

    adds draw to  board

--- DotProjectJava/src/userint/Board.java
+++ DotProjectJava/src/userint/Board.java
@@ -2,17 +2,35 @@ package userint;
 //board class is the actual display of the game
 
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.geometry.Bounds;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Ellipse;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.geometry.Bounds;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.scene.paint.Paint;
 
 import java.util.ArrayList;
 import java.util.List;
 import dots.*;
 
-public class Board extends Pane{
+public class Board extends Application{
 	
 	private userDot uDot;
 	private List<computerDot> dots;
@@ -29,6 +47,40 @@ public class Board extends Pane{
 		initBoard(stage);
 		startGame();
 	}
+	
+public static void main(String[] args) {
+		
+		launch(args);
+		
+	}
+public void start(Stage primaryStage) throws Exception {
+	// TODO Auto-generated method stub
+	dots = new ArrayList<computerDot>();
+	k = new Keyboard();
+	Pane canvas = new Pane();
+	Scene scene = new Scene(canvas, 500, 500);
+	Circle ball = new Circle(30);
+    ball.relocate(0, 10);
+    
+    canvas.getChildren().add(ball);
+    
+    primaryStage.setTitle("Moving Ball");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+    
+    Bounds bounds = canvas.getBoundsInLocal();
+   
+    KeyValue keyV = new KeyValue(ball.layoutXProperty(), bounds.getMaxX()-ball.getRadius());
+    KeyFrame k = new KeyFrame(Duration.seconds(5), keyV);
+    Timeline timeline = new Timeline(k);
+
+/**
+* new KeyFrame(Duration.seconds(3), 
+            new KeyValue(ball.layoutXProperty(), bounds.getMaxX()-ball.getRadius()))
+*/
+    timeline.setCycleCount(3);
+    timeline.play();
+}
 	
 	private void initBoard(Stage stage) {
 		Pane root = new Pane();
